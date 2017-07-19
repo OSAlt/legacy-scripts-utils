381c0ddc919fb2c2e889532166ffee56d59cf28c
Samir Faci <samir@esamir.com>
2017-07-19 13:22:11 -0500

    Adding Flake8 support

--- skillsParser/models/user.py
+++ skillsParser/models/user.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 
 class User(object):
     _data = {}
+
     def __init__(self, raw):
         self._date = raw.get('Timestamp')
         self._email = raw.get('Email Address')
@@ -16,9 +17,9 @@ class User(object):
         self._coder = raw.get('Would you be interested in coding for us?', 'No') == 'Yes'
         self._help_with = raw.get('What would you like to help with?', '')
 
-        self.translation_map = {'N/A': 0, 'Beginner (1-2)': 1, 'Intermediate (3-5)': 3, 'Advanced':4, 'I am a GOD!': 5}
+        self.translation_map = {'N/A': 0, 'Beginner (1-2)': 1, 'Intermediate (3-5)': 3, 'Advanced': 4, 'I am a GOD!': 5}
 
-        self.languages= {}
+        self.languages = {}
         if self.coder:
             self.languages['Java'] = raw.get('Java')
             self.languages['PHP'] = raw.get('PHP')
@@ -51,10 +52,6 @@ class User(object):
         return self._previous_experience
 
     @property
-    def email(self):
-        return self._email
-
-    @property
     def date(self):
         return self._date
 
@@ -62,11 +59,10 @@ class User(object):
     def discord(self):
         return self._discord
 
-
     def __create_user(self, engine):
-        ## Create the baseline user if he doesn't exist.
+        # Create the baseline user if he doesn't exist.
         create_user_query = text("""INSERT into global_data.users (user_email, creation_date, discord) VALUES
-  (:email, :creation_date, :discord ) ON CONFLICT do NOTHING """ )
+  (:email, :creation_date, :discord ) ON CONFLICT do NOTHING """)
         engine.execute(create_user_query,
                        creation_date=self.date,
                        email=self.email,
@@ -78,19 +74,18 @@ class User(object):
             SET skills_details =:skills_details,
                 availability = :availability,
                 coder = :coder,
-                previous_experience = :previous_experience""" )
+                previous_experience = :previous_experience""")
 
         engine.execute(query,
                        creation_date=self.date,
                        email=self.email,
-                       ## This is is far from perfect but it seems to work.
+                       # This is is far from perfect but it seems to work.
                        availability=self.availability,
                        coder=self.coder,
                        skills_details=self.skills,
                        previous_experience=self.previous_expr)
 
         return self.email
-
 
     def __create_user_languages(self, engine, user_email):
         if not self.coder:
@@ -114,25 +109,19 @@ class User(object):
                            skillset=self.translation_map.get(skill, 0),
                            description=skill)
 
-
     def __create_user_categories(self, engine, user_email, categories):
         query = text("""DELETE FROM skills.user_categories where user_email=:user_email""")
         engine.execute(query, user_email=user_email)
         for c in categories:
             query = text("""INSERT into skills.user_categories (user_email, user_category) VALUES (:user_email, :category) 
-                            ON CONFLICT DO NOTHING """ )
+                            ON CONFLICT DO NOTHING """)
             engine.execute(query, user_email=user_email, category=c.strip())
 
-
     def persist(self, engine):
-           user_email = self.__create_user(engine)
-           print('created user: {}'.format(user_email))
-           help = self._help_with
-           items = self._help_with.split(',')
-           if(len(items)) > 0:
-                self.__create_user_categories(engine, user_email, items)
+        user_email = self.__create_user(engine)
+        print('created user: {}'.format(user_email))
+        items = self._help_with.split(',')
+        if (len(items)) > 0:
+            self.__create_user_categories(engine, user_email, items)
 
-           self.__create_user_languages(engine, user_email)
-
-
-
+        self.__create_user_languages(engine, user_email)
